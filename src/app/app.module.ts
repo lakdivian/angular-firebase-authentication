08adb4e633626fb1713c2c07c7e6224391f1a90c
Noorul.km <noorul.km@people10.com>
2019-10-04 16:14:04 +0530

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { MatToolbarModule } from '@angular/material';
+import { RouterModule, Routes } from '@angular/router'; 
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppNavBarComponent } from './components/app-nav-bar/app-nav-bar.component';
+import { LoginComponentComponent } from './components/login-component/login-component.component';
+import { Route } from '@angular/compiler/src/core';
+
+const routes: Routes = [
+  { path: '', redirectTo: "/", pathMatch: "full"},
+  { path: 'login', component: LoginComponentComponent},
+  { path: '**', redirectTo: "/" }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppNavBarComponent,
+    LoginComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFontAwesomeModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    RouterModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
